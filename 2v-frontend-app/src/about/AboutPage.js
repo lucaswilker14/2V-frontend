ecc55e524033ea6aaf30a0d78887a34af58c97f6
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2018-11-28 00:28:46 -0300

    Not Found Page

---
+++ 2v-frontend-app/src/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class AboutPage extends Component {
+  render() {
+    return (
+      <div className="d-flex justify-content-around" style={{ margin: "100px" }}>
+        <p>Programação 3</p>
+        <p>Disciplina do curso de ciencia da computação</p>
+        <p>UFCG</p>
+      </div>
+    )
+  }
+}
