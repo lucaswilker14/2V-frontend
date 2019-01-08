8d6600beb7b0e01211e4b0770fc000e92cbb0ebb
Lucas Wilker <lucas.barbosa@ccc.ufcg.edu.br>
2019-01-08 12:06:51 -0300

    Correcting URL

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-export const API_ROUTE_HEROKU_LOCAL = 'http://localhost:3000/api/2V';
-export const API_ROUTE_HEROKU_HEROKU = 'https://app-2v-backend.herokuapp.com/api/2V';
+export const API_ROUTE_LOCAL = 'http://localhost:3000/api/2V';
+export const API_ROUTE_HEROKU = 'https://app-2v-backend.herokuapp.com/api/2V';
